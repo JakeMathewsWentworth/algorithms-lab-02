b65b7e1ccce786986a6c1523f5bdbd15cb6388c5
Jake Mathews <mathewsj2@wit.edu>
2018-08-15 13:42:37 -0400

    Upload

--- src/edu/wit/cs/comp3370/LAB2.java
+++ src/edu/wit/cs/comp3370/LAB2.java
@@ -1,6 +1,9 @@
 package edu.wit.cs.comp3370;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -12,16 +15,106 @@ import java.util.ArrayList;
  * 
  */
 
+class Heap {
+	public Float value;
+	public Heap leftChild;
+	public Heap rightChild;
+	
+	public int getSize() {
+		int size = 1;
+		if(leftChild != null) {
+			size += leftChild.getSize();
+		}
+		if(rightChild != null) {
+			size += rightChild.getSize();
+		}
+		return size;
+	}
+}
 
 public class LAB2 {
 	
 	//TODO: document this method
-	public static float heapAdd(float[] a) {
-		//TODO: implement this method
+	public static float heapAdd(float[] values) {
+		Arrays.sort(values);
+		List<Float> list = new ArrayList<>();
+		for(float value : values) {
+			list.add(0, value);
+		}
+		Heap heap = buildHeap(list);
+		while(heap.getSize() > 1) {
+			Float sum = 0f;
+			Heap min1 = extractMinFromHeap(heap);
+			if(min1.value != null) {
+				sum += min1.value;
+			}
+			min1.value = null;
+			Heap min2 = extractMinFromHeap(heap);
+			if(min2.value != null) {
+			  sum += min2.value;
+			}
+			min2.value = null;
+			heap = addToHeap(heap, sum);
+		}
 		
-		return -1;
+		return heap.value;
+	}
+	
+	private static Heap buildHeap(List<Float> list) {
+		if(list.isEmpty()) {
+			return null;
+		}
+		Heap heap = new Heap();
+		heap.value = list.remove(0);
+		heap.leftChild = buildHeap(list.subList(0, list.size()/2));
+		heap.rightChild = buildHeap(list.subList(list.size()/2, list.size()));
+		return heap;
+	}
+	
+	private static Heap extractMinFromHeap(Heap heap) {
+		if(heap == null) {
+			return null;
+		}
+		Heap rightMin = extractMinFromHeap(heap.rightChild);
+		if(rightMin != null) {
+			if(rightMin.value != null) {
+				return rightMin;
+			} else {
+				heap.rightChild = null;
+			}
+		}
+		Heap leftMin = extractMinFromHeap(heap.leftChild);
+		if(leftMin != null) {
+			if(leftMin.value != null) {
+				return leftMin;
+			} else {
+				heap.leftChild = null;
+			}
+		}
+		return heap;
 	}
 
+	private static Heap addToHeap(Heap heap, Float value) {
+		if(heap.value != null && heap.value < value) {
+			Heap newHeap = new Heap();
+			newHeap.rightChild = heap;
+			return newHeap;
+		}
+		if(heap.rightChild != null && heap.rightChild.value != null && heap.rightChild.value > value) {
+			addToHeap(heap.rightChild, value);
+		} else if(heap.leftChild != null && heap.leftChild.value != null && heap.leftChild.value > value) {
+			addToHeap(heap.leftChild, value);
+		} else {
+			if(heap.rightChild == null) {
+				heap.rightChild = new Heap();
+				heap.rightChild.value = value;
+			} else if (heap.leftChild == null) {
+				heap.leftChild = new Heap();
+				heap.leftChild.value = value;
+			}
+		}
+		return heap;
+	}
 	/********************************************
 	 * 
 	 * You shouldn't modify anything past here
